a5f9661a7f381c5e3d71cdc131d9ee35db803ea5
Betsy Vidal <ing.betsyvies@gmail.com>
2018-04-09 06:26:00 -0500

    arrangements

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -4,7 +4,6 @@ function searchForm(data) {
     const searchText = document.getElementById('searchText');
     const submitButton = document.getElementById('submitButton')
     const containerItems = document.getElementById('containerItems');
-    submitButton.classList.add('disabled')
 
     var item = data.map(function(i) { return i.title });
     new Awesomplete(searchText, { 
@@ -17,45 +16,49 @@ function searchForm(data) {
         e.preventDefault();
         text = searchText.value;
 
-        if ( text.length > 2) {
-            submitButton.classList.remove('disabled')
+        const awesompleteList = document.getElementById('awesomplete_list_1');
+        const numChildNodes = awesompleteList.childNodes.length
+        
+        newArr = []
+        for (var i = 0; i < numChildNodes; i++) {
+            text = awesompleteList.childNodes[i].textContent;
+            newArr.push(text);
+         }
+        
+        containerItems.innerHTML = '';
+        data.map(items => {
+            newArr.map(text => {
+                if (text === items.title) {
+                    const containerItem = document.createElement('div')
+                    containerItem.setAttribute('key', `${items.id}`);
+                    containerItem.className = 'pure-u-1-3';
 
-            const awesompleteList = document.getElementById('awesomplete_list_1');
-            const numChildNodes = awesompleteList.childNodes.length
+                    const containerImg = document.createElement('img')
+                    containerImg.setAttribute('src', `${items.image}`)
+
+                    const containerTitle = document.createElement('h2')
+                    const title = document.createTextNode(`${items.title}`);
+
+                    const containerText = document.createElement('p')
+                    const teaser= document.createTextNode(`${items.teaser}`);
             
-            newArr = []
-            for (var i = 0; i < numChildNodes; i++) {
-                text = awesompleteList.childNodes[i].textContent;
-                newArr.push(text);
-             }
-            
-            containerItems.innerHTML = '';
-            data.map(items => {
-                newArr.map(text => {
-                    if (text === items.title) {
-                        const containerItem = document.createElement('div')
-                        containerItem.setAttribute('key', `${items.id}`);
-                        containerItem.className = 'pure-u-1-3';
-    
-                        const containerImg = document.createElement('img')
-                        containerImg.setAttribute('src', `${items.image}`)
-    
-                        const containerTitle = document.createElement('h2')
-                        const title = document.createTextNode(`${items.title}`);
-    
-                        const containerText = document.createElement('p')
-                        const teaser= document.createTextNode(`${items.teaser}`);
-                
-                        containerTitle.appendChild(title)
-                        containerText.appendChild(teaser);
-    
-                        containerItem.appendChild(containerImg);
-                        containerItem.appendChild(containerTitle);
-                        containerItem.appendChild(containerText );
-                        containerItems.appendChild(containerItem);
-                   }
-                })
-            });
-        }
+                    containerTitle.appendChild(title)
+                    containerText.appendChild(teaser);
+
+                    containerItem.appendChild(containerImg);
+                    containerItem.appendChild(containerTitle);
+                    containerItem.appendChild(containerText );
+                    containerItems.appendChild(containerItem);
+               }
+            })
+        });
     });
+
+    searchText.addEventListener('keyup', function(e) {
+        if ( searchText.value.length > 2) {
+            submitButton.classList.remove('disabled')
+        } else {
+            submitButton.classList.add('disabled')
+        }
+    })
 }
