89368752eaa0a6985995a20da54c764ad9484463
Betsy Vidal <ing.betsyvies@gmail.com>
2018-04-09 06:40:49 -0500

    Finished arrangements

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -16,42 +16,44 @@ function searchForm(data) {
         e.preventDefault();
         text = searchText.value;
 
-        const awesompleteList = document.getElementById('awesomplete_list_1');
-        const numChildNodes = awesompleteList.childNodes.length
-        
-        newArr = []
-        for (var i = 0; i < numChildNodes; i++) {
-            text = awesompleteList.childNodes[i].textContent;
-            newArr.push(text);
-         }
-        
-        containerItems.innerHTML = '';
-        data.map(items => {
-            newArr.map(text => {
-                if (text === items.title) {
-                    const containerItem = document.createElement('div')
-                    containerItem.setAttribute('key', `${items.id}`);
-                    containerItem.className = 'pure-u-1-3';
-
-                    const containerImg = document.createElement('img')
-                    containerImg.setAttribute('src', `${items.image}`)
-
-                    const containerTitle = document.createElement('h2')
-                    const title = document.createTextNode(`${items.title}`);
-
-                    const containerText = document.createElement('p')
-                    const teaser= document.createTextNode(`${items.teaser}`);
+        if (text.length > 2) {
+            const awesompleteList = document.getElementById('awesomplete_list_1');
+            const numChildNodes = awesompleteList.childNodes.length
             
-                    containerTitle.appendChild(title)
-                    containerText.appendChild(teaser);
-
-                    containerItem.appendChild(containerImg);
-                    containerItem.appendChild(containerTitle);
-                    containerItem.appendChild(containerText );
-                    containerItems.appendChild(containerItem);
-               }
-            })
-        });
+            newArr = []
+            for (var i = 0; i < numChildNodes; i++) {
+                text = awesompleteList.childNodes[i].textContent;
+                newArr.push(text);
+             }
+            
+            containerItems.innerHTML = '';
+            data.map(items => {
+                newArr.map(text => {
+                    if (text === items.title) {
+                        const containerItem = document.createElement('div')
+                        containerItem.setAttribute('key', `${items.id}`);
+                        containerItem.className = 'pure-u-1-3';
+    
+                        const containerImg = document.createElement('img')
+                        containerImg.setAttribute('src', `${items.image}`)
+    
+                        const containerTitle = document.createElement('h2')
+                        const title = document.createTextNode(`${items.title}`);
+    
+                        const containerText = document.createElement('p')
+                        const teaser= document.createTextNode(`${items.teaser}`);
+                
+                        containerTitle.appendChild(title)
+                        containerText.appendChild(teaser);
+    
+                        containerItem.appendChild(containerImg);
+                        containerItem.appendChild(containerTitle);
+                        containerItem.appendChild(containerText );
+                        containerItems.appendChild(containerItem);
+                   }
+                })
+            });
+        }
     });
 
     searchText.addEventListener('keyup', function(e) {
